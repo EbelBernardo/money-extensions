3c3535d9aa0a0987d5fe820b6988191a12f92737
Bernardo Ebel <bernardoebel@gmail.com>
2026-01-14 14:44:06 -0300

    Primeiro commit

---
+++ MoneyExtension.Test/MoneyExtensionTest.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Test;
+
+[TestClass]
+public sealed class MoneyExtensionTest
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
